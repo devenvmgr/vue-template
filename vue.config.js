6981d6a569a4ed4164f9fec228128ec98f12957d
Jason <jason@casjay.net>
2021-11-08 12:00:19 -0500

    🗃 Modified: vue.config.js  🗃

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,7 @@
-//vue.config.js
+// vue.config.js
 module.exports = {
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].title = "My Vue App";
-                return args;
-            })
-    }
-}
+  chainWebpack: (config) => {
+    config
+      .plugin('html');
+  },
+};
