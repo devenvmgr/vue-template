bd38fa15613d44bfda0f75504ba9a2ded6a982fc
Jason <jason@casjay.net>
2021-11-08 11:54:43 -0500

    ❓ Added: vue.config.js  ❓

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
-import About from '../views/About.vue'
+import About from '../views/About.vue';
 
+const BASE_URL = process.env.BASE_URL || '/';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -11,12 +12,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-component: About,
+    component: About,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(BASE_URL),
   routes,
 });
 
